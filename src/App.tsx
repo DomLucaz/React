ea2c2e28200818bbfaf282bc7e5994be87e0b3f1
DomLucaz <contacto.figueiredo@gmail.com>
2021-11-30 15:18:03 -0300

    Added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Navbar from './Components/estaticos/navbar/Navbar';
+import Footer from './Components/estaticos/footer/Footer';
 import './App.css';
 import Home from './paginas/home/Home';
 
 
 function App() {
   return (
+   <>
+  <Navbar />
     <Home />
+    <Footer />
+   </>
   );
 }
 
