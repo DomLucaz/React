1228e876b793ebcad6d8804a6259dc2f91ef92c0
DomLucaz <contacto.figueiredo@gmail.com>
2021-12-12 23:26:55 -0300

    Added models and Services

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,50 @@
-import React from 'react';
-import Navbar from './Components/estaticos/navbar/Navbar';
-import Footer from './Components/estaticos/footer/Footer';
-import './App.css';
-import Home from './paginas/home/Home';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./Components/estaticos/navbar/Navbar";
+import Footer from "./Components/estaticos/footer/Footer";
+import CadastroUsuario from "./paginas/cadastrousuario/CadastroUsuario";
+import "./App.css";
+import Home from "./paginas/home/Home";
+import Login from "./paginas/login/Login";
+import ListaPostagem from "./Components/postagens/listapostagem/ListaPostagem";
+import ListaTema from "./Components/temas/listatemas/ListaTema";
 
 
 function App() {
   return (
-   <>
-  <Navbar />
-    <Home />
-    <Footer />
-   </>
+    <Router>
+      <Navbar />
+      <Switch>
+        <div style={{ minHeight: "100vh" }}>
+
+          <Route exact path="/ ">
+            <Login />
+          </Route>
+
+          <Route path= '/login'>
+            <Login />
+            </Route>
+
+          <Route path="/home">
+            <Home />
+          </Route>
+          
+          <Route path="/cadastrousuario">
+            <CadastroUsuario />
+          </Route>
+
+          <Route path="/temas">
+            <ListaTema />
+          </Route>
+
+          <Route path="/posts">
+            <ListaPostagem />
+          </Route>
+        </div>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
 export default App;
-
---
+++ src/paginas/login/Login.tsx
@@ -0,0 +1,111 @@
+import React, { useState, useEffect, ChangeEvent } from "react";
+import { Grid, Box, Typography, TextField, Button } from "@material-ui/core";
+import { Link, useHistory } from "react-router-dom";
+import useLocalStorage from "react-use-localstorage";
+import { login } from "../../services/Service";
+import UserLogin from "../../models/UserLogin";
+import "./Login.css";
+
+function Login() {
+  let history = useHistory();
+  const [token, setToken] = useLocalStorage("token");
+  const [userLogin, setUserLogin] = useState<UserLogin>({
+    id: 0,
+    usuario: "",
+    senha: "",
+    token: "",
+  });
+
+  function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+    setUserLogin({
+      ...userLogin,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  async function onSubmit(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    try {
+      await login("/usuarios/logar", userLogin, setToken)
+
+
+      alert("Usuario logado com sucesso!");
+    } catch (error) {
+      alert("Dados do usuário inconsistentes. Erro ao logar!");
+    }
+  }
+
+    useEffect(()=>{
+      if(token != ''){
+        history.push('/home')
+      }
+    }, [token])
+
+  return (
+    <Grid container direction="row" justifyContent="center" alignItems="center">
+      <Grid alignItems="center" xs={6}>
+        <Box paddingX={20}>
+          <form onSubmit={onSubmit}>
+            <Typography
+              variant="h3"
+              gutterBottom
+              color="textPrimary"
+              component="h3"
+              align="center"
+              className="textos1"
+            >
+              Entrar
+              <TextField
+                value={userLogin.usuario}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                id="usuario"
+                label="usuario"
+                variant="outlined"
+                name="usuario"
+                margin="normal"
+                fullWidth
+              />
+              <TextField
+                value={userLogin.senha}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => updatedModel(e)}
+                id="senha"
+                label="senha"
+                variant="outlined"
+                name="senha"
+                margin="normal"
+                type="password"
+                fullWidth
+              />
+              <Box marginTop={2} textAlign="center">
+                <Button type="submit" variant="contained" color="primary">
+                  Logar
+                </Button>
+              </Box>
+            </Typography>
+          </form>
+          <Box display="flex" justifyContent="center" marginTop={2}>
+            <Box marginRight={1}>
+              <Typography variant="subtitle1" gutterBottom align="center">
+                Não tem uma conta?
+              </Typography>
+            </Box>
+            <Link to="/castrousuario">
+              <Typography
+                variant="subtitle1"
+                gutterBottom
+                align="center"
+                className="textos1"
+              >
+                Cadastra-se
+              </Typography>
+            </Link>
+          </Box>
+        </Box>
+      </Grid>
+      <Grid xs={6} className="imagem"></Grid>
+    </Grid>
+  );
+}
+
+export default Login;
